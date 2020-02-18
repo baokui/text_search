fa86c85b05b8cc541179c6845c383e89ba7e495d
baokui <sy880115>
2020-02-18 17:38:09 +0800

    LM

---
+++ languageModel/statisticLM/mapper1.py
@@ -0,0 +1,6 @@
+import sys
+for data in sys.stdin:
+    data = data.strip()
+    fields = data.split('\t')
+    if int(fields[1])>1:
+        sys.stdout.write("%s\n" % data)
--- languageModel/statisticLM/run_main.sh
+++ languageModel/statisticLM/run_main.sh
@@ -1,6 +1,6 @@
 mkdir log
-date=20200103
-time=(00 01 02 03 04 05 06 07 08 09 10 11)
+date=20200104
+time=(12 13 14 15 16 17 18 19 20 21 22 23)
 for((i=0;i<12;i++))
 do
 input=VpaOutput_guobk/data_LM/$date/${time[$i]}/*
@@ -8,7 +8,15 @@ output=VpaOutput_guobk/data_LM/ngram/$date/${time[$i]}
 nohup sh run_join.sh $input $output >> ./log/$date-${time[$i]}.log 2>&1 &
 done
 
-date=20200102
+date=20200104
 input=VpaOutput_guobk/data_LM/ngram/$date/*/p*
 output=VpaOutput_guobk/data_LM/ngram_day/$date
 nohup sh run_join_join.sh $input $output >> ./log/day-$date.log 2>&1 &
+
+input=VpaOutput_guobk/data_LM/ngram_day/*/p*
+output=VpaOutput_guobk/data_LM/ngram_all
+nohup sh run_join_join.sh $input $output >> ./log/day-$date.log 2>&1 &
+
+input=VpaOutput_guobk/data_LM/ngram_all/p*
+output=VpaOutput_guobk/data_LM/ngram_all1
+nohup sh run_filter.sh $input $output >> ./log/day-$date.log 2>&1 &
