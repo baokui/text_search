dfb2eea0ea29490b97ef5ef1a96daad2ef3156d2
baokui <sy880115>
2020-03-10 12:32:44 +0800

    extract feature

--- getData/user_behavior_eachday_imagemix/mapper_user8.py
+++ getData/user_behavior_eachday_imagemix/mapper_user8.py
@@ -3,18 +3,19 @@ import time
 for data in sys.stdin:
     data = data.strip()
     fields = data.split('\t')
-    if len(fields) != 11:
+    if len(fields) != 10:
         continue
     [_, Time, userid, rc, AC, query, CI, iC, Post, _, dialogue] = fields
     if userid[-1]!='8':
         continue
-    if rc == 'imagemix':
+    if rc == 'godText':
         try:
             t1 = time.strptime(Time[:20], '%d/%b/%Y:%H:%M:%S')
             Time = time.strftime('%Y-%m-%d %H:%M:%S',t1)
-            # idx = Post.find('[seq]')
-            # sc = Post[:idx]
-            sc = Post.split('[seq]')[0]
+            #idx = Post.find('[seq]')
+            #sc = Post[:idx]
+            #sc = Post.split('[seq]')[0]
+            sc = 'sc_null'
             S = [userid,Time,AC,query,sc]
             S = '\t'.join(S)
             sys.stdout.write("%s\n" % S)
