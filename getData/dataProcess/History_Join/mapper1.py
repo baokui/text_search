48fe8b66051925bc0dac340d0cf544907fc45245
baokui <sy880115>
2020-03-10 21:12:56 +0800

    extract feature

--- getData/dataProcess/History_Join/mapper1.py
+++ getData/dataProcess/History_Join/mapper1.py
@@ -5,7 +5,7 @@ import os
 #[last_session_id,Time,userid,rc,'#'.join(AC),query,CI,iC,Post,F]
 filepath = os.environ.get('mapreduce_map_input_file')
 filename = os.path.split(filepath)[0].strip()
-idx = filename.find('userhistory_imagemix/user_')+len('userhistory_imagemix/user_')+2
+idx = filename.find('userhistory_godText/user_')+len('userhistory_godText/user_')+2
 for data in sys.stdin:
     data = data.strip()
     fields = data.split('\t')
