b3b7bf5f72a6dd739fb19a18d5628800147dc836
baokui <sy880115>
2020-03-11 22:04:42 +0800

    extract feature

--- getData/modelTrain/run_train_model_train.sh
+++ getData/modelTrain/run_train_model_train.sh
@@ -1,17 +1,20 @@
-sleep 2h
 rootpath=/search/odin/guobk/streaming/vpa/text_search/getData/modelTrain
 path_global=$rootpath/data/feature_global/20200309/
 path_user=$rootpath/data/feature_user/20200309/
 path_session0=$rootpath/data/feature_session/202003
-date=(09 08 07 06)
+date=(08 07 06 09)
 modelversion=model20200309
 resultpath=$rootpath/$modelversion
 model=lr
 mkdir $resultpath
 mode=train
 mkdir log
+for((epoch=0;epoch<10;epoch++))
+do
 for((i=0;i<${#date[@]};i++))
 do
 path_session=$path_session0/${date[$i]}
+echo $epoch for $path_session
 python -u train_model.py $mode $path_global $path_user $path_session $resultpath $model >> log/$modelversion-$mode.log
+done
 done
