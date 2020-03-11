66c0172b769ffbdac371bbe464e71500cc1668ae
baokui <sy880115>
2020-03-11 21:22:23 +0800

    extract feature

--- getData/modelTrain/train_model.py
+++ getData/modelTrain/train_model.py
@@ -25,7 +25,7 @@ class Config_train(object):
         self.epochs = 1
         self.CKPT_path = 'ckpt'
         self.step_saveckpt = 100
-        self.testlines = 5000000
+        self.testlines = 1000000
 config_model = modelconfig()
 Sc = config_model.get_sc()
 def data_initial(path_global,path_user,user,Sc,resultpath):
@@ -113,115 +113,50 @@ def train(path_global,path_user,path_session, resultpath,model,joining=False):
         completed += 1
         KK = len(data_train) - completed
         print('training used time %0.2f mins and needs about %0.2f mins for the rest users'%((time.time()-time0)/60,(time.time()-time0)/60*KK))
-def test(path_global,path_user,path_session,resultpath,model,path_ckpt='',path_userData='userData',joining=False,user_idx='a'):
-    ####################################################
-    config_train = Config_train()
+def test(path_global,path_user,path_session, resultpath,model,joining=False):
+    config_train=Config_train()
+    config_model=modelconfig()
     config_train.feature_dim = config_model.get_nb_features()[0]
     if joining:
         config_train.feature_dim+=1
     tf.reset_default_graph()
-    if model == 'lr':
-        X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr(
-            config_train)
-    elif model == 'lr-dense':
-        X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr_dense(
-            config_train)
-    global_step = tf.train.get_or_create_global_step()
-    train_op = tf.group(train_op, [tf.assign_add(global_step, 1)])
-    saver = tf.train.Saver()
+    if model=='lr':
+        X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr(config_train)
+    elif model=='lr-dense':
+        X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr_dense(config_train)
+    saver = tf.train.Saver(max_to_keep=10)
     session = tf.Session()
-    if len(path_ckpt)==0:
-        path_ckpt = os.path.join(resultpath, config_train.CKPT_path)
-        if not os.path.exists(path_ckpt):
-            os.mkdir(path_ckpt)
-        ckpt_file = tf.train.latest_checkpoint(path_ckpt)
-        print('ckpt file is %s' % ckpt_file)
-        if ckpt_file:
-            saver.restore(session, ckpt_file)
-        else:
-            return
+    path_ckpt = os.path.join(resultpath,config_train.CKPT_path)
+    if not os.path.exists(path_ckpt):
+        print('no model')
+        return
+    ckpt_file = tf.train.latest_checkpoint(path_ckpt)
+    if ckpt_file:
+        saver.restore(session, ckpt_file)
     else:
-        print('ckpt file is %s'%path_ckpt)
-        ckpt_file = path_ckpt
-        if ckpt_file:
-            saver.restore(session, ckpt_file)
-        else:
-            return
-
-    ckpt_backup = os.path.join(resultpath, "ckpt_backup")
-    if not os.path.exists(ckpt_backup):
-        os.mkdir(ckpt_backup)
-    tmpfile = ckpt_file
-    cfile = tmpfile + ".*"
-    cmdstr = "cp " + cfile + " " + ckpt_backup
-    os.system(cmdstr)
-
-    idx = ckpt_file.find('model.ckpt-')+len('model.ckpt-')
-    if not os.path.exists(os.path.join(resultpath,'test')):
-        os.mkdir(os.path.join(resultpath,'test'))
-    predictpath0 = os.path.join(resultpath,'test','predict'+ckpt_file[idx:])
-
+        print('no model')
+        return
     learning_rate_ = config_train.learning_rate
+    step = 0
+    completed = 0
     data_train = os.listdir(path_session)
-    auc = 0
-    auc_each = []
-    predictpaths = []
+    usernb = 0
     for path in data_train:
-        predictpath = predictpath0 + '-' + path + '.txt'
-        datapath = modules.get_sessionfile(os.path.join(path_session, path))
-        users = [path]
-        r_all, r_sc_all, r_time_all, D_user = data_initial(path_global, path_user, users,path_userData)
+        usernb += 1
+        datapath = modules.get_sessionfile(os.path.join(path_session,path))
+        print('training on '+ path)
+        time0 = time.time()
+        users = path
+        r_all, r_sc_all, r_time_all, D_user = data_initial(path_global,path_user,users,Sc,resultpath)
         D_other = [r_all] + r_sc_all + r_time_all
-        iter = modules.Data_test(datapath, D_user,D_other, r_sc_all, r_time_all, user_idx=user_idx,batch_size = config_train.test_batch_size, punc=config_model.punc,
-                                       max_line=config_train.testlines, config_global=config_model,joining=joining)
+        iter = modules.iterData(datapath, D_user, D_other, r_sc_all, r_time_all,batch_size=config_train.testlines, rate_skip = config_train.skip_rate,rate_skip_neg=0,config_global=config_model,joining=joining)
         data = next(iter)
-        #y_p = []
-        #user = []
-        #y = []
-        nb_batch=0
-        f_write = open(predictpath, 'w+')
-        while data!="__STOP__":
-            X, y0, userbatch =  data
-            y0 = np.array(y0)
-            y0 = np.reshape(y0, (len(y0), 1))
-            X_test = X
-            y_test = y0
-            y_p0 = session.run(predict_y, feed_dict={X_holder: X_test, y_holder: y_test, learning_rate: learning_rate_})
-            #y_p.append(y_p0)
-            #y.append(y0)
-            #user.extend(userbatch)
-            print(nb_batch,config_train.testlines/config_train.test_batch_size)
-            #y_p = np.concatenate(y_p)
-            #y = np.concatenate(y)
-            tmp = ['\t'.join([userbatch[ii], str(int(y0[ii][0])), str(y_p0[ii][0])]) for ii in range(len(y0))]
-            f_write.write('\n'.join(tmp))
-            f_write.write('\n')
-            data = next(iter)
-            nb_batch += 1
-        auctmp = modules.getAUC(predictpath)
-        auc_each.append('%0.4f'%auctmp)
-        auc += auctmp
-        predictpaths.append(predictpath)
-        print('testing on file %s with auc = %0.4f and ckptfile is %s'%(os.path.join(path_session, path),auctmp,ckpt_file))
-    auc = auc/len(data_train)
-    print('testing on path %s with average auc = %0.4f and ckptfile is %s'%(path_session, auc,ckpt_file))
-    if os.path.exists(os.path.join(resultpath, 'test-auc-ckpt.txt')):
-        with open(os.path.join(resultpath, 'test-auc-ckpt.txt'), 'r') as f:
-            stmp = f.read().strip().split('\n')
-        auc0 = float(stmp[-1].split('\t')[0])
-    else:
-        stmp = []
-        auc0 = 0
-    if auc > auc0:
-        stmp.append('%0.4f' % auc + '\t' + '\t'.join(auc_each) + '\t' + tmpfile)
-        with open(os.path.join(resultpath, 'test-auc-ckpt.txt'), 'w') as f:
-            f.write('\n'.join(stmp))
-    else:
-        tmpfile = tmpfile.replace('/ckpt/', '/ckpt_backup/')
-        os.remove(tmpfile + ".meta")
-        os.remove(tmpfile + ".index")
-        os.remove(tmpfile + ".data-00000-of-00001")
-    return auc,predictpaths
+        x0, y0 = data
+        feed_dict = {X_holder: x0, learning_rate: learning_rate_}
+        yp = session.run(predict_y,feed_dict=feed_dict)
+        yp = [yp[i][0] for i in range(len(yp))]
+        auc = modules.calAUC(prob=yp, labels=y0)
+        print('ckpt is {} with auc is {}'.format(ckpt_file,auc))
 def predict(path_global,path_user,path_session,resultpath,model,path_ckpt='',path_userData='userData',joining=False,user_idx='a'):
     ####################################################
     auc,predictpaths=test(path_global,path_user,path_session,resultpath,model,path_ckpt,path_userData,joining,user_idx)
@@ -233,3 +168,5 @@ if __name__=='__main__':
     path_global, path_user, path_session, resultpath, model = sys.argv[2:]
     if mode=='train':
         train(path_global,path_user,path_session, resultpath,model)
+    if mode=='test':
+        test(path_global,path_user,path_session, resultpath,model)
