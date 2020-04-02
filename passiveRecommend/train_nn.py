22296224f178c9096aa5fa0a06ede161792e7f14
baokui <sy880115>
2020-04-02 18:00:27 +0800

    extract feature

--- passiveRecommend/train_nn.py
+++ passiveRecommend/train_nn.py
@@ -30,9 +30,9 @@ tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 
 # Training parameters
 tf.flags.DEFINE_integer("train_batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 200, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 200, "Save model after this many steps (default: 100)")
-tf.flags.DEFINE_integer("log_every", 50, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("evaluate_every", 1000, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 1000, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("log_every", 100, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
@@ -190,8 +190,8 @@ def train(x_train, y_train, tokenizer, x_dev, y_dev):
                 step, summaries, loss, accuracy,predict = sess.run(
                     [global_step, dev_summary_op, cnn.loss, cnn.accuracy,cnn.predictions],
                     feed_dict)
-                y0 = [np.argmax(tt) for tt in y_batch]
-                auc = calAUC(predict, y0)
+                y_p = [tmp[0] for tmp in predict]
+                auc = calAUC(y_p, y_batch)
                 time_str = datetime.datetime.now().isoformat()
                 print("{}: step {}, loss {:g}, acc {:g}, auc {:g}".format(time_str, step, loss, accuracy,auc))
                 if writer:
