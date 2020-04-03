1a38f303b818a2c3f1ddf8582af595da7c6bb0bf
baokui <sy880115>
2020-04-03 19:27:04 +0800

    extract feature

--- passiveRecommend/test.py
+++ passiveRecommend/test.py
@@ -6,6 +6,7 @@ import numpy as np
 import os
 import json
 import sys
+from train import getW2V
 def testing(path_test,config_feature,path_ckpt,config_train,mode='lr'):
     with open(path_test, 'r') as f:
         S = f.read().strip().split('\n')
@@ -15,10 +16,7 @@ def testing(path_test,config_feature,path_ckpt,config_train,mode='lr'):
     print('number of positive/negative samples of testSet is {}/{}'.format(sum(yTst), len(yTst) - sum(yTst)))
     feature_dim = len(XTst[0])
     config_train.feature_dim =feature_dim
-    if mode=='lr':
-        X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr(feature_dim)
-    if mode=='lr-dense':
-        X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr_dense(config_train)
+    X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy = simple_lr(feature_dim)
     global_step = tf.train.get_or_create_global_step()
     train_op = tf.group(train_op, [tf.assign_add(global_step, 1)])
     saver = tf.train.Saver(max_to_keep=10)
@@ -37,7 +35,7 @@ def testing(path_test,config_feature,path_ckpt,config_train,mode='lr'):
     X = ['\t'.join(['预测值','实际值','文本'])]
     for i in range(len(S)):
         X.append('%0.4f'%y_p[i]+'\t'+S[i][1]+'\t'+S[i][0])
-    with open('data/test_predict.txt','w') as f:
+    with open('data/test_predict_'+mode+'.txt','w') as f:
         f.write('\n'.join(X))
     thr0 = [0.1*i for i in range(10)]
     R = ['\t'.join(['阈值','准确率','精度','召回率'])]
@@ -52,25 +50,43 @@ def testing(path_test,config_feature,path_ckpt,config_train,mode='lr'):
         rec = float(TP)/(TP+FN)
         R.append('\t'.join(['%0.1f'%thr,'%0.4f'%acc,'%0.4f'%pre,'%0.4f'%rec]))
         print([thr,acc,pre,rec])
-    with open('data/test_result.txt','w') as f:
+    with open('data/test_result-'+mode+'.txt','w') as f:
         f.write('\n'.join(R))
 def main(mode):
+    path_train = 'data/train.txt'
     path_test = 'data/test.txt'
     path_idf = 'data/idf_char.json'
     path_vocab = 'data/vocab.txt'
-    path_ckpt = mode+'-ckpt'
+    path_idf_word = 'data/idf_word.json'
+    path_vocab_word = 'data/vocab_word.txt'
+    path_w2v = '/search/odin/guobk/streaming/vpa/word2vec128/model-mean'
+    path_ckpt = mode + '-ckpt'
     config_feature = {}
-    config_feature['use_charIdf'] = True
     config_feature['use_sentLen'] = True
     config_feature['use_puncExist'] = True
-    config_feature['use_char'] = True
+    if 'lr' in mode:
+        config_feature['use_charIdf'] = True
+        config_feature['use_char'] = True
+        with open(path_idf, 'r') as f:
+            idf = json.load(f)
+        with open(path_vocab, 'r') as f:
+            vocab = f.read().strip().split('\n')
+        config_feature['idf'] = idf
+        config_feature['charList'] = vocab
+    if 'w2v' in mode:
+        config_feature['use_w2v'] = True
+        config_feature['w2v'] = getW2V(path_w2v)
+        config_feature['dim_v'] = 128
+    if 'word' in mode:
+        config_feature['use_wordIdf'] = True
+        config_feature['use_word'] = True
+        with open(path_idf_word, 'r') as f:
+            idf = json.load(f)
+        with open(path_vocab_word, 'r') as f:
+            vocab = f.read().strip().split('\n')
+        config_feature['idf_word'] = idf
+        config_feature['wordList'] = vocab
     config_train = Config_train()
-    with open(path_idf,'r') as f:
-        idf = json.load(f)
-    with open(path_vocab,'r') as f:
-        vocab = f.read().strip().split('\n')
-    config_feature['idf'] = idf
-    config_feature['charList'] = vocab
     testing(path_test, config_feature, path_ckpt, config_train,mode=mode)
 if __name__=='__main__':
     mode = sys.argv[1]
