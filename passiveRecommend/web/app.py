504c9bfc76ee54e0622de3c3dfaea1701fd763bc
baokui <sy880115>
2020-04-13 13:19:21 +0800

    extract feature

--- passiveRecommend/web/app.py
+++ passiveRecommend/web/app.py
@@ -5,8 +5,11 @@ import sys
 from datetime import datetime
 import time
 import logging
+import jieba
+from train import getW2V
 from Config import config_predict,Config_train,getConfig_feature
 from modelpredict import getModel,predicting
+from demo_modelpredict import predict
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 port = 5000
@@ -17,7 +20,17 @@ ConfigPredict = config_predict()
 ConfigTrain = Config_train()
 ConfigFeature= getConfig_feature(ConfigPredict)
 ConfigTrain.feature_dim = ConfigPredict.feature_dim
-X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy,session = getModel(ConfigTrain,ConfigPredict)
+path_config='../demo/ModelConfig.json'
+path_w2v='../demo/w2v.file'
+# 导入模型各参赛
+with open(path_config,'r') as f:
+    config = json.load(f)
+config['weight_w'] = [float(t) for t in config['weight_w']]
+config['weight_b'] = float(config['weight_b'])
+config['threshold'] = float(config['threshold'])
+# 导入词向量
+w2v = getW2V(path_w2v)
+#X_holder, y_holder, learning_rate, predict_y, loss, optimizer, train_op, grads, accuracy,session = getModel(ConfigTrain,ConfigPredict)
 @app.route('/api/gen', methods=['POST'])
 def test2():
     r = request.json
@@ -25,7 +38,10 @@ def test2():
     try:
         p = 0.0
         result = 0
-        p,result = predicting(data, ConfigFeature, X_holder, y_holder, learning_rate, predict_y, session, thr=ConfigPredict.thr)
+        words = list(jieba.cut(data))
+        #p,result = predicting(data, ConfigFeature, X_holder, y_holder, learning_rate, predict_y, session, thr=ConfigPredict.thr)
+        p = predict(data, words, config, w2v)
+        result = int(p>config['threshold'])
         p = '%0.4f'%p
         result = str(result)
         app.logger.info("input:{}".format(data))
