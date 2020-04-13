769f73387981fe65f7b2aff8b700eef72f634340
baokui <sy880115>
2020-04-13 18:37:32 +0800

    extract feature

--- passiveRecommend/test.py
+++ passiveRecommend/test.py
@@ -54,7 +54,7 @@ def testing(path_test,config_feature,path_ckpt,config_train,mode='lr',name=''):
         acc = float(TP+TN)/len(yp)
         pre = float(TP)/(TP+FP)
         rec = float(TP)/(TP+FN)
-        R.append('\t'.join(['%0.1f'%thr,'%0.4f'%acc,'%0.4f'%pre,'%0.4f'%rec]))
+        R.append('\t'.join(['%0.2f'%thr,'%0.4f'%acc,'%0.4f'%pre,'%0.4f'%rec]))
         print('\n'.join(R))
     print('auc=%0.4f'%auc)
     R.append('auc=%0.4f'%auc)
