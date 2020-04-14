64e39b0e03963b71c897fc43ff11c456019e7edf
baokui <sy880115>
2020-04-14 19:40:37 +0800

    extract feature

---
+++ passiveRecommend/app_modelconfig.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+from flask import Flask, request, Response
+import json
+import sys
+import logging
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+port = int(sys.argv[1])
+path_config = sys.argv[2]
+with open(path_config,'r') as f:
+    config = json.load(f)
+@app.route('/api/gen', methods=['POST'])
+def test2():
+    try:
+        response = config
+        response['message'] = 'sucess'
+    except Exception as e:
+        app.logger.error("error:",e)
+        response = {'message': 'error'}
+    response_pickled = json.dumps(response)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+# start flask app
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
